febb5c3492df84dbbff84b50aeedb60ab7314a8a
Ignacio Fleming <ignacioflemings@gmail.com>
2023-07-21 15:53:11 -0300

    ejercicio object keys

--- index.js
+++ index.js
@@ -1,67 +1,38 @@
-class Contador {
-  constructor(nombre) {
-    this.nombre = nombre;
-    this.contador = 0;
+const objetos = [
+  {
+    manzanas: 3,
+    peras: 2,
+    carne: 1,
+    jugos: 5,
+    dulces: 2,
+  },
+  {
+    manzanas: 1,
+    sandias: 1,
+    huevos: 6,
+    jugos: 1,
+    panes: 4,
+  },
+];
+
+const listaProductos = [];
+objetos.map((element) => {
+  const keys = Object.keys(element);
+
+  keys.map((key) => {
+    if (!listaProductos.includes(key)) {
+      listaProductos.push(key);
+    }
+  });
+});
+console.log(listaProductos);
+
+const totalVendido = objetos.reduce((acc, objeto) => {
+  for (const propiedad in objeto) {
+    acc += objeto[propiedad];
+    console.log(objeto[propiedad]);
   }
-  static contadorGlobal = 0;
-  getResponsable() {
-    return this.nombre;
-  }
-  contar() {
-    this.contador++;
-    Contador.contadorGlobal++;
-  }
-  getCuentaIndividual() {
-    return this.contador;
-  }
-  getCuentaGlobal() {
-    return Contador.contadorGlobal;
-  }
-}
+  return acc;
+}, 0);
 
-const persona1 = new Contador("pepito");
-const persona2 = new Contador("Lautaro");
-const persona3 = new Contador("Fermin");
-const persona4 = new Contador("Antonio");
-const persona5 = new Contador("Carlos");
-
-persona1.contar();
-persona1.contar();
-persona1.contar();
-persona2.contar();
-persona2.contar();
-persona3.contar();
-persona3.contar();
-persona3.contar();
-persona3.contar();
-persona3.contar();
-persona3.contar();
-persona3.contar();
-persona4.contar();
-persona4.contar();
-persona4.contar();
-persona4.contar();
-persona4.contar();
-persona5.contar();
-persona1.contar();
-persona3.contar();
-
-console.log(`El responsable es ${persona1.getResponsable()}`);
-console.log(`El contador individual es ${persona1.getCuentaIndividual()}`);
-console.log(`La cuenta global al momento es ${persona1.getCuentaGlobal()}`);
-
-console.log(`El responsable es ${persona2.getResponsable()}`);
-console.log(`El contador individual es ${persona2.getCuentaIndividual()}`);
-console.log(`La cuenta global al momento es ${persona2.getCuentaGlobal()}`);
-
-console.log(`El responsable es ${persona3.getResponsable()}`);
-console.log(`El contador individual es ${persona3.getCuentaIndividual()}`);
-console.log(`La cuenta global al momento es ${persona3.getCuentaGlobal()}`);
-
-console.log(`El responsable es ${persona4.getResponsable()}`);
-console.log(`El contador individual es ${persona4.getCuentaIndividual()}`);
-console.log(`La cuenta global al momento es ${persona4.getCuentaGlobal()}`);
-
-console.log(`El responsable es ${persona5.getResponsable()}`);
-console.log(`El contador individual es ${persona5.getCuentaIndividual()}`);
-console.log(`La cuenta global al momento es ${persona5.getCuentaGlobal()}`);
+console.log(totalVendido);

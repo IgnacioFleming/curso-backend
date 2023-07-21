45db38712951850cb07c2c09e52d8836639b7267
Ignacio Fleming <ignacioflemings@gmail.com>
2023-07-21 17:12:29 -0300

    hands on lab

--- index.js
+++ index.js
@@ -1,38 +1,74 @@
-const objetos = [
-  {
-    manzanas: 3,
-    peras: 2,
-    carne: 1,
-    jugos: 5,
-    dulces: 2,
-  },
-  {
-    manzanas: 1,
-    sandias: 1,
-    huevos: 6,
-    jugos: 1,
-    panes: 4,
-  },
-];
+class TicketManager {
+  #precioBasedeGanancia = 0.15;
 
-const listaProductos = [];
-objetos.map((element) => {
-  const keys = Object.keys(element);
-
-  keys.map((key) => {
-    if (!listaProductos.includes(key)) {
-      listaProductos.push(key);
-    }
-  });
-});
-console.log(listaProductos);
-
-const totalVendido = objetos.reduce((acc, objeto) => {
-  for (const propiedad in objeto) {
-    acc += objeto[propiedad];
-    console.log(objeto[propiedad]);
+  constructor() {
+    this.eventos = [];
   }
-  return acc;
-}, 0);
+  getEventos() {
+    return this.eventos;
+  }
+  agregarEvento(
+    nombre,
+    lugar,
+    precio,
+    capacidad = 50,
+    fecha = new Date().toLocaleDateString()
+  ) {
+    const evento = {
+      nombre,
+      lugar,
+      precio,
+      capacidad,
+      fecha,
+      participantes: [],
+    };
+    if (this.eventos.length === 0) {
+      evento.id = 1;
+    } else {
+      evento.id = this.eventos[this.eventos.length - 1].id + 1;
+    }
+    this.eventos.push(evento);
+  }
 
-console.log(totalVendido);
+  agregarUsuario(idEvento, idUsuario) {
+    const indiceEvento = this.eventos.findIndex((e) => e.id === idEvento);
+    const participantesEvento =
+      this.eventos[indiceEvento].participantes.includes(idUsuario);
+    if (indiceEvento === -1) {
+      console.log("El evento que quiere referenciar no existe");
+      return;
+    }
+    if (participantesEvento) {
+      console.log("El usuario ya está registrado en este evento");
+      return;
+    }
+    this.eventos[indiceEvento].participantes.push(idUsuario);
+  }
+
+  ponerEventoEnGira(idEvento, nuevaLocalidad, nuevaFecha) {
+    const evento = this.eventos.find((e) => e.id === idEvento);
+    const eventoEnGira = {
+      ...evento,
+      lugar: nuevaLocalidad,
+      fecha: nuevaFecha,
+      participantes: [],
+      id: this.eventos[this.eventos.length - 1].id + 1,
+    };
+    this.eventos.push(eventoEnGira);
+  }
+}
+
+const manejadorDeEventos = new TicketManager();
+manejadorDeEventos.agregarEvento("Guns&Roses", "Buenos Aires", 10000);
+manejadorDeEventos.agregarEvento("Ricardo Arjona", "Cordoba", 45000);
+manejadorDeEventos.agregarEvento("Adele", "Salta", 80000);
+
+manejadorDeEventos.agregarUsuario(1, "A22");
+manejadorDeEventos.agregarUsuario(2, "B22");
+manejadorDeEventos.agregarUsuario(3, "C22");
+manejadorDeEventos.agregarUsuario(1, "A24");
+manejadorDeEventos.agregarUsuario(1, "A25");
+
+manejadorDeEventos.ponerEventoEnGira(3, "Paris", "22/10/2024");
+
+console.log(manejadorDeEventos.getEventos());

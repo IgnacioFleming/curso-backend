2fb9c63ca88a45cd31807f1535725516c2a0b955
Ignacio Fleming <ignacioflemings@gmail.com>
2023-07-23 15:26:33 -0300

    finalizo el desafio con testing

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Desafío Clases con ECMAScript y ECMAScript avanzado
+
 class ProductManager {
   constructor() {
     this.products = [];
@@ -32,42 +34,40 @@ class ProductManager {
   getProducts() {
     return this.products;
   }
+
+  getProductById(productId) {
+    const productFound = this.products.find(
+      (element) => element.id === productId
+    );
+    return productFound || "Not Found";
+  }
 }
+
+//Testing Entregable
 
 const PM1 = new ProductManager();
 
-const discos = [
-  "Discos",
-  "Discos de Vinilo de las bandas más iconicas de la Historia",
-  50000,
-  "https://keepthemspinning.com/es/wp-content/uploads/sites/2/2023/02/experiencia-pink-floyd-discos-de-vinilo-Keep-Them-Spinning.png",
-  "A0001",
-  10,
-];
+console.log(PM1.getProducts());
 
-const guitarrasElectricas = [
-  "Guitarras Electricas",
-  "Guitarras Yamaha con equipamiento completo",
-  120000,
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_NKkdIicRYoZHoF5OFBYSIQdSu4lxmiGBTA&usqp=CAU",
-  "A0002",
-  22,
-];
-
-//Creo producto con code repetido.
-
-const parlantes = [
-  "Parlantes Profesionales",
-  "Parlantes profesionales para instalar en complejos de recreamiento",
-  75000,
-  "https://www.ecelectronica.com.ar/6965-large_default/parlante-bluetooth-portatil-etheos-usb-radio-fm-aux-20-watts.jpg",
-  "A0002",
-  5,
-];
-
-PM1.addProduct(...discos);
-PM1.addProduct(...guitarrasElectricas);
-//Agrego el producto con code repetido para que salte la validacion.
-PM1.addProduct(...parlantes);
+PM1.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "sin imagen",
+  "abc123",
+  25
+);
 
 console.log(PM1.getProducts());
+
+PM1.addProduct(
+  "producto prueba",
+  "Este es un producto prueba",
+  200,
+  "sin imagen",
+  "abc123",
+  25
+);
+
+console.log(PM1.getProductById(1));
+console.log(PM1.getProductById(2));

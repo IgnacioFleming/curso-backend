bf31f97020170d48c16f7b573e7e425b93150748
Ignacio Fleming <ignacioflemings@gmail.com>
2023-09-05 20:24:42 -0300

    actualizo cart router

--- src/routes/carts.fs.js
+++ src/routes/carts.fs.js
@@ -2,36 +2,48 @@ import { Router } from "express";
 import CartManager from "../dao/FileSystem/cartManager.fs.js";
 
 const router = Router();
-const CM = new CartManager("carts.json", "products.json");
+const CM = new CartManager();
 
-router.post("/", (req, res) => {
-  CM.createCart().then(({ status, description }) =>
-    res.send({ status, description })
-  );
+router.post("/", async (req, res) => {
+  try {
+    const { status, payload } = await CM.createCart();
+    res.send({ status, payload });
+  } catch (error) {
+    res.status(500).send({ status: "error", description: error });
+  }
 });
 
-router.get("/:cid", (req, res) => {
-  const cid = parseInt(req.params.cid);
+router.get("/:cid", async (req, res) => {
+  try {
+    const cid = parseInt(req.params.cid);
+    const { status, description, payload } = await CM.getCartById(cid);
 
-  CM.getCartById(cid).then(({ status, description }) => {
     if (status === "success") {
-      res.send({ status, description });
+      res.send({ status, payload });
     } else {
       res.status(400).send({ status, description });
     }
-  });
+  } catch (error) {
+    res.status(500).send({ status: "error", description: error });
+  }
 });
 
-router.post("/:cid/products/:pid", (req, res) => {
-  const cid = parseInt(req.params.cid);
-  const pid = parseInt(req.params.pid);
-  CM.addProductToCart(cid, pid).then(({ status, description }) => {
+router.post("/:cid/products/:pid", async (req, res) => {
+  try {
+    const cid = parseInt(req.params.cid);
+    const pid = parseInt(req.params.pid);
+    const { status, description, payload } = await CM.addProductToCart(
+      cid,
+      pid
+    );
     if (status === "success") {
-      res.send({ status, description });
+      res.send({ status, payload });
     } else {
       res.status(400).send({ status, description });
     }
-  });
+  } catch (error) {
+    res.status(500).send({ status: "error", description: error });
+  }
 });
 
 export default router;

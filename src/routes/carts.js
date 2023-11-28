2b68e2ec408a01e23b90f3d2b24b7a966eab8b7e
Ignacio Fleming <ignacioflemings@gmail.com>
2023-11-27 21:00:45 -0300

    documentado hasta updateProductsOfCart

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -5,20 +5,20 @@ import { applyPolicy } from "../middlewares/errors/policies/policies.js";
 
 const router = Router();
 
-router.post("/", cartsController.createCart);
+router.post("/", passportCall("jwt"), applyPolicy(["PUBLIC"]), cartsController.createCart);
 
-router.get("/:cid", cartsController.getCartById);
+router.get("/:cid", passportCall("jwt"), applyPolicy(["PUBLIC"]), cartsController.getCartById);
+
+router.delete("/:cid", passportCall("jwt"), applyPolicy(["USUARIO", "PREMIUM"]), cartsController.resetCart);
+
+router.put("/:cid", passportCall("jwt"), applyPolicy(["USUARIO", "PREMIUM"]), cartsController.updateProductsOfCart);
 
 router.post("/:cid/products/:pid", passportCall("jwt"), applyPolicy(["USUARIO", "PREMIUM"]), cartsController.addProductToCart);
 
 router.delete("/:cid/products/:pid", passportCall("jwt"), applyPolicy(["USUARIO", "PREMIUM"]), cartsController.deleteProductFromCart);
 
-router.put("/:cid", passportCall("jwt"), applyPolicy(["USUARIO", "PREMIUM"]), cartsController.updateProductsOfCart);
-
 router.put("/:cid/products/:pid", passportCall("jwt"), applyPolicy(["USUARIO", "PREMIUM"]), cartsController.updateProductQuantityFromCart);
 
-router.delete("/:cid", passportCall("jwt"), applyPolicy(["USUARIO", "PREMIUM"]), cartsController.resetCart);
-
-router.post("/:cid/purchase", cartsController.confirmPurchase);
+router.post("/:cid/purchase", passportCall("jwt"), applyPolicy(["USUARIO", "PREMIUM"]), cartsController.confirmPurchase);
 
 export default router;

fc8e80abbcc01c01f60fe824bf49b0bafd51e511
Ignacio Fleming <ignacioflemings@gmail.com>
2023-12-28 20:07:08 -0300

    error alert added to login

--- src/utils.js
+++ src/utils.js
@@ -48,12 +48,20 @@ export const passportCall = (strategy) => {
         }`);
         return res.redirect("/forgottenPass");
       }
-      if (!user) {
+      if (!user && strategy === "login") {
         req.logger.warning(`{
           status: "error",
           error: ${info.message ? info.message : info.toString()},
         }`);
         return res.send({ status: "error", error: info.message ? info.message : info.toString() });
+      }
+      if (!user) {
+        req.logger.warning(`{
+          status: "error",
+          error: ${info.message ? info.message : info.toString()},
+        }`);
+
+        return res.redirect("/login");
       }
       req.user = user;
       next();
